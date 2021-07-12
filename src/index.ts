f20a94ea50a9dad7e4267715f848c56707e5acac
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-12 15:02:00 +0700

    Add `DocumentRenderKit` context entry

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './common';
 export * from './content-status';
 export * from './context';
 export * from './context-of';
+export * from './document-render-kit';
 export * from './drek.ns';
 export * from './fragment';
 export * from './placement';
